37ab57ebbc1c07fb0670adbbc298809c6427f9af
allison cortez <allizetorc@gmail.com>
2019-11-01 18:53:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,10 @@ class Song
   
   def self.find_or_create_by_name(name)
     if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else 
+      self.create_by_name(name)
+    end
   end
   
 end
