46c6dff7b584dc367fa35d04af0d639dccd2c41a
allison cortez <allizetorc@gmail.com>
2019-11-01 20:24:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   
   def self.new_from_filename(filename)
     split_filename = filename.chomp(".mp3").split(" - ")
-    song = Song.new 
+    song = self.new 
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
     song
